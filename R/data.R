05c2c84565a876ed672f41f583c55a7d858ca5ad
trick track triu <tricktracktriu@mailbox.org>
2023-02-06 16:50:54 +0100

    add data to pkg

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Abfallmengen nach Abfallarten und Gemeinden Kanton Zuerich
+#'
+#' Source: https://www.zh.ch/de/politik-staat/opendata.zhweb-noredirect.zhweb-cache.html?keywords=ogd#/datasets/1803@awel-kanton-zuerich/distributions/3460
+#' @format
+#' Ein Datensatz mit 6 Variablen und 34668 Zeilen:
+#' \describe{
+#'    \item{Jahr}{Jahr der Erhebung}
+#'    \item{Gemeinde_Nr}{Kantonale Gemeinde ID}
+#'    \item{Gemeinde}{Name der Gemeinde}
+#'    \item{Abfallart}{Spezifizierung des Abfalles}
+#'    \item{Wert}{numerische Mengenangabe}
+#'    \item{Einheit}{Masseinheit des Wertes}
+#' }
+"abfall_zh"
