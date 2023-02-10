3d1b3cce43951de3c4bb259f62bd3e61ce37515e
trick track triu <tricktracktriu@mailbox.org>
2023-02-10 15:15:35 +0100

    update

--- inst/sandbox.R
+++ inst/sandbox.R
@@ -4,6 +4,7 @@ library(devtools)
 library(tidyverse)
 library(lubridate)
 library(socviz)
+library(biviz)
 load_all()
 #library(socviz)  # Book: Data Visualization by Kieran Healy, https://github.com/kjhealy/socviz
 
@@ -25,33 +26,44 @@ tech_stocks <-
 # test amounts ------------------------------------------------------------
 
 gss_lon |>
-  mutate(count = n()) |>
-  #plot_amounts_vertical(degree, count)
-  #plot_amounts_vertical(count, degree)
+  #mutate(n = n()) |>
+  count(degree) |>
+  plot_amounts_vertical(degree, n)
+  #plot_amounts_vertical(n, degree)
   #plot_amounts_horizontal(degree, count)
-  #plot_amounts_facets(age, count, degree)
-  #plot_amounts_grouped(sex, wtssall, degree)
-  count(degree, sex) |>
-  ## group_by(degree) |>
-  ## mutate(count_by_group = n()) |>
-  ## ungroup() |> select(degree, count_by_group, sex)
+  #count(age, degree) |>
+  #plot_amounts_facets(age, n, degree)
+  #count(sex, degree) |>
+ #plot_amounts_grouped(x = degree, y = n, group = sex) #+
+  #scale_fill_discrete_sequential(palette = "Purples 2")
   plot_amounts_stacked(x = degree, n, sex)
+
+  gss_lon |>
+    mutate(count = n()) |>
+    count(degree, sex) |>
+    group_by(degree, sex) |>
+    filter(n() > 1) |>
+    nrow() < 1
 
 # test distributions ------------------------------------------------------
 
 gss_lon|>
-  filter(happy != "NA") |>
+  #filter(happy != "NA") |>
   #drop_na() |>
   #count(age)
-  #plot_distributions_histogram(as.numeric(age), bw = 3)
-  #plot_distributions_density(as.numeric(age), bw = 3)
-  #plot_distributions_sidebyside(as.numeric(age), group = sex, bw = 3)
-  #plot_distributions_grouped(as.numeric(age), group = happy, bw = 5)
+  #mutate(age = as.character(age)) |>
+  #plot_distributions_histogram(age)#, bw = 3)
+  #mutate(age = as.character(age)) |>
+  #plot_distributions_density(age, bw = 3)
+  #plot_distributions_sidebyside(x = age, group = sex)
+  drop_na(happy) |>
+  plot_distributions_grouped(age, group = happy, bw = 3)
 
-gss_lon |>
-  filter(!is.na(happy)) |>
-  plot_distributions_raincloud(age, happy)
-  #plot_distributions_boxplot(age, happy, size = 3)
+gss_lon |># nrow()
+  #filter(!is.na(happy)) |>
+  filter(row_number() %% 100 == 1) |>
+  #plot_distributions_raincloud(y = age, happy)
+  plot_distributions_boxplot(y = age, x = happy, size = 3)
 
 
 
@@ -63,53 +75,77 @@ mpg |>
   mutate(sum_year = sum(n)) |>
   group_by(manufacturer, year) |>
   summarise(percent = n / sum_year) |>
-  ungroup()#|>
+  ungroup() |>
   # group_by(year) |>
   # summarise(sum = sum(percent))
-  # plot_proportions_sidebyside(x = manufacturer, percent = percent, facet = year)
+  plot_proportions_sidebyside_bar(x = manufacturer, percent = percent, facet = year)
 
 gss_lon |>
+  #count(age, happy) |>
   # müssen dichte plots immer eine numerische x achse haben?
-  plot_proportions_sidebyside_density1(x = as.numeric(age), group = happy)
-  #plot_proportions_sidebyside_density1(x = as.character(age), group = happy)
+  #plot_proportions_sidebyside_density1(x = age, group = happy)
+  #mutate(age = as.character(age)) |>
+  plot_proportions_sidebyside_density1(x = age, group = happy)
   #plot_proportions_sidebyside_density1(x = age, group = happy)
 
 # age = labelled -> prüfungs muss in der funktion noch implementiert werden
-gss_lon |>
+socviz::gss_lon |>
   mutate(age = as.numeric(age)) |>
   select(age, happy) |>
   filter(!is.na(happy)) |>
   plot_proportions_sidebyside_density2(x = age, group = happy)
 
 gss_sib |>
-  group_by(year, sex) |>
-  summarise(n = n()) |>
+  #filter(year == 1980) |>
+  #group_by(year, sex) |>
+  #summarise(n = n()) |>
+  count(year, sex) |>
   group_by(year) |>
   mutate(sum_year = sum(n)) |>
   group_by(year, sex) |>
   summarise(percent = n / sum_year) |>
-plot_proportions_stacked(year, percent, sex)
+  ungroup() |> #pull(year) |> is.numeric()
+  #mutate(year = as.factor(year)) |>
+  #plot_proportions_bar(group = sex)
+  plot_proportions_stacked(x = year, y = percent, group = sex)
+  #plot_proportions_sidebyside_col(x = year, y = percent, color = sex, facet = sex)
+
 
 mpg |>
-  group_by(year) |>
-  summarise(n = n()) |>
+  # group_by(year) |>
+  # reframe(n = n(),
+  #         manufacturer = manufacturer) |>
+  count(year) |>
   ungroup() |>
-  mutate(percent = n / sum(n)) |>
-  plot_proportions_donut(group = year)
-  #plot_proportions(n = percent, year)
+  mutate(percent = n / sum(n)) |> #glimpse()
+  #plot_proportions_donut(group = year)
+  plot_proportions_bar(year)
+  facet_wrap(vars(manufacturer))
 
 
 
 # test timeseries ---------------------------------------------------------
 
-df_temp <-
-  mpg |>
-  #mutate(year = as.factor(year)) |>
+ggplot2::mpg |>
   group_by(year, manufacturer) |>
   summarise(hwy_by_manufacturer = mean(hwy)) |>
-  ungroup() #|>
+  ungroup() |>
+  plot_timeseries_slope(x = year, y = hwy_by_manufacturer, group = manufacturer)
 
-plot_timeseries_slope(data = df_temp, x = year, y = hwy_by_manufacturer, group = manufacturer)
+ts_tbl(EuStockMarkets) |>
+  mutate(time = as_date(time)) |>
+  #filter(id != "FTSE") |>
+  #filter(id == "FTSE") |>
+  #plot_timeseries_line(x = time, y = value, group = id)
+  #plot_timeseries_trend(x = time, y = value) + facet_wrap(vars(id))
+  mutate(time = format(time, "%Y-%m")) |>
+  group_by(id, time) |>
+  summarise(value = mean(value)) |>
+  ungroup() |>
+  plot_timeseries_detrend(x = time, y = value, group = id)
+
+
+
 
 tech_stocks |>
   select(company, date, price_indexed) |>
@@ -129,12 +165,12 @@ tech_stocks |>
   # mutate(date = year(date)) |>
   mutate(date = format(date, "%Y-%m")) |>
   # #mutate(date = as.yearmon(date, "%Y %m")) |>
-  filter(company == "Apple") |>
+  #filter(company == "Apple") |>
   #group_by(date) |>
   group_by(company, date) |>
   summarise(price_indexed = mean(price_indexed)) |>
   ungroup() |>
-  #plot_timeseries_detrend(x = date, y = price_indexed, method = "pc", group = company, facet = TRUE)
+  plot_timeseries_detrend(x = date, y = price_indexed, group = company) #, facet = FALSE)#, group = company, facet = TRUE)
   ## wenn nur eine gruppe
   plot_timeseries_detrend(x = date, y = price_indexed, method = "pc", group = company, facet = FALSE)
 
@@ -202,3 +238,13 @@ p <- ggplot(data = midwest,
 p + geom_density(alpha = 0.3)
 p + geom_line(stat = "density")
 
+
+# 06.02.2023 --------------------------------------------------------------
+
+stock_markets <- tsbox::ts_tbl(datasets::EuStockMarkets) |>
+  dplyr::mutate(time = lubridate::as_date(time))
+stock_markets |>
+  dplyr::filter(id != "FTSE") |>
+  plot_timeseries_trend(x = time, y = value) +
+  # Erweiterung mit ggplot2 Funktion
+  ggplot2::facet_wrap(ggplot2::vars(id))
